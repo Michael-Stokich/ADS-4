53b7ae0c84974cdb77352bf8bd316b7257c70a93
Misha Stokitch <119692913+Michael-Stokich@users.noreply.github.com>
2025-04-11 13:41:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,50 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int ct = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        ct++;
+      }
+    }
+  }
+  return ct;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int lt = 0;
+  int rt = len - 1;
+  int ct = 0;
+  while (lt < rt) {
+    int sum = arr[lt] + arr[rt];
+  if (sum == value) {
+    ct++;
+    lt++;
+    rt--;
+    } else if (sum < value) {
+  lt++;
+  } else {
+  rt--;
+  }
+    }
+  return ct;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int ct = 0;
+  for (i = 0 i < len; i++) {
+    int tg = value - arr[i];
+    int lt = i + 1;
+    int rt = len - 1;
+    while (lt <= rt) {
+      int m = lt + (rt - lt) / 2;
+      if (arr[m] == tg) {
+        ct++;
+        break;
+      } else if (arr[m] < tg) {
+        lt = m + 1;
+      } else {
+        rt = m - 1;
+      }
+    }
+  }
+  return count;
 }
