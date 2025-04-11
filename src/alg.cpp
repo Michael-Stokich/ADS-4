f51c1cbd16a323c63140f12e0f0d10a5fd09e68f
Misha Stokitch <119692913+Michael-Stokich@users.noreply.github.com>
2025-04-11 14:16:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,14 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int ct = 0;
-  for (int i = 0; i < len; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        ct++;
-      }
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
     }
-  }
-  return ct;
+    return count;
 }
 
 int countPairs2(int *arr, int len, int value) {
